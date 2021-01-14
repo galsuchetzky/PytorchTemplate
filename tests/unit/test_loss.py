ef5057849ca0d6af95eebafb5aa986c8a5a8eec0
WIN10 <galsuchetzky@gmail.com>
2021-01-14 21:08:07 +0200

    added testing template to all unittest files

---
+++ tests/unit/test_loss.py
@@ -0,0 +1,15 @@
+import unittest
+
+"""
+TODO testing:
+1) 
+"""
+
+
+class TestClassName(unittest.TestCase):
+    def test_(self):
+        self.assertIsNotNone(None)
+
+
+if __name__ == '__main__':
+    unittest.main()
